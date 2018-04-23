c4a806748668be97eb89f50eaac57548e33ec963
Liang <pwrliang@gmail.com>
2018-04-23 00:24:57 -0400

    generalize the code to support more app stores

--- app_crawler.py
+++ app_crawler.py
@@ -1,12 +1,12 @@
 from file_parser import parse_list, parse_keyvalue_new, parse_filterwords
-from request_wrapper import get_html, encode_url_app_page, encode_url_main_page, get_url_root
-from html_parser import parse_app_list, parse_app_details
-from difflib import SequenceMatcher
+from request_wrapper import get_html, encode_url_main_page, get_url_root
+from html_parser_baidu import parse_app_list as parse_app_list_baidu, parse_app_details as parse_app_details_baidu
+from html_parser_xiaomi import parse_app_list as parse_app_list_xiaomi, parse_app_details as parse_app_details_xiaomi
 from file_saver import append_file
 import os
 
 
-def search_app(search_entry_url, keyword):
+def search_app(parse_app_list, search_entry_url, keyword):
     request_url = search_entry_url + keyword
     request_url = encode_url_main_page(request_url)
     html_code = get_html(request_url)
@@ -22,8 +22,6 @@ def search_app(search_entry_url, keyword):
     if not app_list:
         return res
 
-    # since, we may found a few Apps relative to the keyword,
-    # we just found the one which most similar to the keyword
     for app_meta_info in app_list:
         app_name = app_meta_info['app_name']
         app_brief = app_meta_info['app_brief']
@@ -34,18 +32,15 @@ def search_app(search_entry_url, keyword):
         if app_detailed_link.startswith('/'):
             app_detailed_link = get_url_root(search_entry_url) + app_detailed_link
 
-        name_similarity = SequenceMatcher(a=app_name, b=keyword).ratio()
-
-        if res is None or name_similarity > res['app_similarity']:
+        if app_name == keyword:
             res = {'app_name': app_name,
                    'app_brief': app_brief,
-                   'app_detailed_link': app_detailed_link,
-                   'app_similarity': name_similarity}
+                   'app_detailed_link': app_detailed_link}
 
     return res
 
 
-def get_app_details(app_details_url):
+def get_app_details(parse_app_details, app_details_url):
     request_url = encode_url_main_page(app_details_url)
     html_code = get_html(request_url)
     app_details = None
@@ -105,25 +100,41 @@ if __name__ == "__main__":
 
     print('Supported appstores: %d' % len(search_entries))
 
+    function_mapper = {'百度手机助手': {'func_parse_app_list': parse_app_list_baidu,
+                                  'func_parse_app_details': parse_app_details_baidu},
+                       '小米应用商店': {'func_parse_app_list': parse_app_list_xiaomi,
+                                  'func_parse_app_details': parse_app_details_xiaomi}}
+
     for appstore_name in search_entries:
         if len(search_entries[appstore_name]) < 1:
             print("%sdomain.txt configure ERROR!" % appstore_name)
             continue
         print('searching from %s' % appstore_name)
+        func_parsers = function_mapper[appstore_name]
+        func_parse_app_list = func_parsers['func_parse_app_list']
+        func_app_details = func_parsers['func_parse_app_details']
 
+        not_found = []
         for keyword in keywords:
-            app_meta_info = search_app(search_entries[appstore_name], keyword)
+            app_meta_info = search_app(func_parse_app_list, search_entries[appstore_name], keyword)
             if not app_meta_info:
                 print('WARN: cannot found: %s' % keyword)
                 append_file(notfoundfile, keyword)
+                not_found.append(keyword)
                 continue
 
             app_info = app_meta_info.copy()
 
-            app_detailed_info = get_app_details(app_meta_info['app_detailed_link'])
+            app_detailed_info = get_app_details(func_app_details, app_meta_info['app_detailed_link'])
             if app_detailed_info:
                 app_info.update(app_detailed_info)
 
-            append_file(resfile, str(app_info))
+            append_file(resfile, '%s\t%s\t%s\t%s' % (
+                app_info['app_name'],
+                app_info['app_brief_long'],
+                app_info['app_detailed_link'],
+                app_info['app_download_url']))
 
             print('proceed: keyword %s found %s' % (keyword, app_info['app_name']))
+
+        keywords = not_found  # we use up this app sotre, we need search keywords from new store
---
+++ html_parser_xiaomi.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+def parse_app_list(html_code):
+    # TODO Referencing html_parser_baidu.py
+    pass
+
+def parse_app_details(html_code):
+    # TODO Referencing html_parser_baidu.py
+    pass
